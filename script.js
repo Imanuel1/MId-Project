25c8a7956b1b4d5f256b47d3946cab39293d24f7
imanuel <toswim100@gmail.com>
2021-03-04 17:02:47 +0200

    fix the JS code (i=3 wasn't include) - commit

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let i = 0;
 function changePicture(){
     let array = ["images/110571325734.jpg", "images/250X110668568400.jpg", "images/banner_atar.jpg" , "images/paz_250x110103131473.jpg"];
     document.getElementById("change-img").src = array[i];
-    i++;
     if(i == 3){
         i=0;
     }
+    i++;
 }
 
 
